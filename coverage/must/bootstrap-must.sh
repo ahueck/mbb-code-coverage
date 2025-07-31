1cb3dbe92cdb7f101985882d5bf0436bf35595f2
Simon Schwitanski <schwitanski@itc.rwth-aachen.de>
2025-07-31 09:37:18 +0200

    Update bootstrap script

--- coverage/must/bootstrap-must.sh
+++ coverage/must/bootstrap-must.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env zsh
 
+# CLAIX-specific script to build MUST with LLVM coverage
 module purge
 module load gompi/2024a LLVM/19 Perl
 
@@ -10,26 +11,23 @@ export OMPI_CXX=clang++
 
 SCRIPTDIR=$PWD
 
-build="true"
-
 # Build MUST with LLVM coverage
 BUILD_DIR=/my/builddir
 INSTALL_DIR=/my/installdir
-TARBALL_DIR=/my/tarballdir/
+TARBALL_DIR=/my/tarballdir
 
-if [[ "$build" == "true" ]]; then
-    # Get MUST tarball and apply patch to enable LLVM coverage
-    cd $TARBALL_DIR
-    wget https://hpc.rwth-aachen.de/must/files/MUST-v1.11.0.tar.gz -O $TARBALL_DIR/MUST-v1.11.0.tar.gz
-    tar -xvzf MUST-v1.11.0.tar.gz
-    cd $TARBALL_DIR/MUST-v1.11.0
-    patch -p1 < $SCRIPTDIR/coverage.patch
-fi
+mkdir -p $TARBALL_DIR
+
+# Get MUST tarball and apply patch to enable LLVM coverage
+cd $TARBALL_DIR
+wget https://hpc.rwth-aachen.de/must/files/MUST-v1.11.0.tar.gz -O $TARBALL_DIR/MUST-v1.11.0.tar.gz
+tar -xvzf MUST-v1.11.0.tar.gz
+cd $TARBALL_DIR/MUST-v1.11.0
+patch -p1 < $SCRIPTDIR/coverage.patch
 
 cd $TARBALL_DIR/MUST-v1.11.0
 
 # Install MUST
-# tar -xf /MUST-v1.10.0.tar.gz && \
 mkdir -p ${BUILD_DIR}
 cmake -S . -B ${BUILD_DIR} \
     -DCMAKE_BUILD_TYPE=Debug \
